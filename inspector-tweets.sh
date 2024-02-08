647dfc609d948723b2bfa83e839adb1efd06cf05
brianreborn <brianisbornagain@gmail.com>
2024-02-08 06:38:16 -0600

    Add an inspector-tweets.sh script which works similarly to the inspector.sh script in populating @<username>/user.json but it simply retrieves all Tweets for that account into the file @<username>/user_tweets_and_replies.json.
    This is the --raw output here which means it is a fully-formed
    JSON document and not a series of newline-separated JSON fragments.

--- inspector-tweets.sh
+++ inspector-tweets.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
 u="$1"
-mkdir -p @"$u"
+mkdir -p @"$u"/tweets
 
 o=@"$u"/user.json
 test ! -f "$o" &&
   twscrape user_by_login "$u" | tee "$o"
 uid=`cut -d' ' -f2 @"$u"/user.json | cut -d , -f1`
 
-o=@"$u"/following.json
+o=@"$u"/user_tweets_and_replies.json
 test ! -f "$o" &&
-  twscrape following $uid > "$o"
-
-o=@"$u"/followers.json
-test ! -f "$o" &&
-  twscrape followers $uid > "$o"
+  twscrape user_tweets_and_replies --raw $uid > "$o"
