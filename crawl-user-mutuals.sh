e4df3f3cdcf620101271cdbfac3d43c9b719b171
brianreborn <brianisbornagain@gmail.com>
2024-02-16 16:06:52 -0600

    Make sure to continue where we left off on the same iteration of this.

--- crawl-user-mutuals.sh
+++ crawl-user-mutuals.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
-sqlite3 -csv twitter_data.db > user-mutuals.csv <<-SQL
+u=user-mutuals.csv
+[ ! -e $u ] && sqlite3 -csv twitter_data.db > $u <<-SQL
 SELECT DISTINCT F1.follower_name
  FROM Followers F1 JOIN Followers F2
  ON F1.follower_name = F2.followee_name AND F2.follower_name = F1.followee_name
  ORDER BY F1.follower_followers_count ASC;
 SQL
-for user in `cat user-mutuals.csv`; do
+for user in `cat $u`; do
 	(cd .. && ./inspector.sh $user)
 	ln -s ../@$user
 done
