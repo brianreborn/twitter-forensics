c990e3dc52ed493d0e80ee2d6f3522219aa00588
brianreborn <brianisbornagain@gmail.com>
2024-02-24 00:53:32 -0600

    Clean up empty directories, not just errored.

--- cleanup-not-founds.sh
+++ cleanup-not-founds.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 mkdir -p Lost+Found
 for i in @*; do
-	if cmp $i/user.json user-not-found.json >/dev/null; then
+	if [ -e $i/user.json -a ! -s $i/user.json ] || cmp $i/user.json user-not-found.json >/dev/null; then
 		mv $i Lost+Found
 	fi
 done	
