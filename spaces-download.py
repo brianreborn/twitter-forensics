4fd164b01a470f2591988aed18de3ed18b0f3d6d
brianreborn <brianisbornagain@gmail.com>
2024-02-28 10:26:30 -0600

    Correct a typo os.environ -> environ.

--- spaces-download.py
+++ spaces-download.py
@@ -2,7 +2,7 @@ from os import environ
 from twitter.account import Account
 from twitter.scraper import Scraper
 
-account = Account(cookies = { "ct0": os.environ['TWITTER_COOKIE_ct0'], "auth_token": os.environ['TWITTER_COOKIE_auth_token'] })
+account = Account(cookies = { "ct0": environ['TWITTER_COOKIE_ct0'], "auth_token": environ['TWITTER_COOKIE_auth_token'] })
 scraper = Scraper(session = account.session)
 rooms = ['spaces id values...']
 spaces = scraper.spaces(rooms = rooms, audio = True, chat = True)
