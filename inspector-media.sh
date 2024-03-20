95aaf6591502b47c777edee11f45ace11b006e4b
brianreborn <brianisbornagain@gmail.com>
2024-03-20 12:16:03 +0000

    Symlink in appropriate media files into the @userdir.

--- inspector-media.sh
+++ inspector-media.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
-u="$1"
-inspector-tweets.sh "$u"
-jq -r "..|.media_url_https?|strings" < "@$u/user_tweets_and_replies.json" |
-	xargs wget -mi
+workdir=`pwd`
+export PATH=$PATH:$workdir
+for u in $@; do
+	inspector-tweets.sh $u
+	m=inspector-media-$u.txt
+	jq -r "..|.media_url_https?|strings" < @$u/user_tweets_and_replies.json |
+		tee $m |
+		xargs wget -mi
+	# mirror the subdirectories but link the actual media files
+	cut -d/ -f3- < $m | (cd @$u &&
+		while read relarch; do
+			relarchsubdir=`dirname $relarch`
+			(mkdir -p $relarchsubdir &&
+				cd $relarchsubdir &&
+				ln -s $workdir/$relarch)
+		done)
+done
