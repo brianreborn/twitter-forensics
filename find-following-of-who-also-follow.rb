11b17bd13aadcdbe27d622a4e71d8ab212543c4c
brianreborn <brianisbornagain@gmail.com>
2024-04-01 14:23:02 -0500

    Deal with both Following and Followers the same.

--- find-following-of-who-also-follow.rb
+++ find-following-of-who-also-follow.rb
@@ -7,32 +7,35 @@ def json_fragments(filename)
   File.readlines(filename).collect {|json| JSON.parse(json)}
 end
 
-def user_follows(username)
+def user_following(username)
   return json_fragments("@#{username}/following.json")
 end
 
-target_follows = user_follows(following_of)
+def user_followers(username)
+  return json_fragments("@#{username}/followers.json")
+end
+
+target_follows = user_following(following_of)
 # map followed Twitter username to map of {following username: User JSON object}
 who_follows = {}
 
 who_also_follow.each {|who|
   wf = who_follows[who] = {}
-  user_follows(who).each {|f| wf[f["username"]] = f}
+  # either following or follower. could do mutuals (AND) instead.
+  user_following(who).each {|f| wf[f["username"]] = f}
+  user_followers(who).each {|f| wf[f["username"]] = f}
 }
 
 results_by_username = target_follows.collect {|who_target_follows|
   results = []
   wtfu = who_target_follows["username"]
   who_also_follow.each {|waf|
-#    p "trying #{wtfu}"
-#    p who_follows[waf][wtfu]
     if who_follows[waf][wtfu]
       results.push(waf)
     end
   }
   next if results.empty?
   results.unshift(wtfu, who_target_follows["id_str"]) # prepend username,id_str
-  p results
 }.keep_if {|x| x}
 
 results_by_username.each {|res|
